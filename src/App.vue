e696b3461fd0f50e0f5c6df7fd4b03ec9d965fbe
imblackline <yaghoobnezhad77@gmail.com>
2025-05-28 12:01:44 +0200

    Complete the header

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,32 @@
 import { RouterLink, RouterView } from "vue-router";
 import { useI18n } from "vue-i18n";
 import { useTheme } from "./composables/useTheme";
+import { computed } from "vue";
+import logo from '@/assets/images/logo.png';
 
 const { t, locale } = useI18n();
-const { toggleTheme } = useTheme();
+const { toggleTheme, theme } = useTheme();
+
+const themeIcon = computed(() => (theme.value === "dark" ? "sun" : "moon"));
 </script>
 
 <template>
-  <header
-    class="navbar is-transparent is-flex is-align-content-center is-justify-content-center"
-  >
-    <div class="navbar-item is-size-2">
-      {{ t("Repositories") }}
+  <header class="navbar is-transparent is-flex is-align-items-center">
+    <div class="navbar-item is-flex-grow-1 ml-6 is-justify-content-center">
+      <div class="is-flex is-align-items-center">
+        <figure class="image mr-3 is-flex is-align-items-center">
+          <img :src="logo" alt="Logo" class="is-fullwidth is-fullheight" />
+        </figure>
+        <div class="is-size-3 has-text-centered">
+          {{ t("Repositories") }}
+        </div>
+      </div>
     </div>
-    <div class="navbar-end">
+    <div class="navbar-start">
       <div class="navbar-item">
         <button class="button is-text" @click="toggleTheme" aria-label="Toggle theme">
-          <span class="icon">
-            <font-awesome-icon icon="coffee" />
+          <span class="icon is-large">
+            <font-awesome-icon :icon="themeIcon" size="lg" />
           </span>
         </button>
       </div>
@@ -27,4 +36,5 @@ const { toggleTheme } = useTheme();
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
